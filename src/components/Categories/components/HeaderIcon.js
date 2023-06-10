b1c33497bda674b9618c7bc4a425e5c43aba468a
Oliver Tacke <o.tacke@posteo.de>
2023-06-10 12:02:01 +0200

    Use FontAwesome instead of custom assets

--- src/components/Categories/components/HeaderIcon.js
+++ src/components/Categories/components/HeaderIcon.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import Thumb from './../../../../assets/thumb.svg';
 
 function HeaderIcon() {
   return (
     <span
       className={'h5p-discussion-header-icon'}
     >
-      <img
-        alt={''} // Merely decoration
-        src={Thumb}
-      />
     </span>
   );
 }
